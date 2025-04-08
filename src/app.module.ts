92b3c8d48271332562100d256d76c1aa754afad7
Máté Barbér <mate.barber@digitalthinkers.com>
2025-04-09 01:04:15 +0200

    feat: add typeorm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from '@app/app.controller';
 import AppConfig from '@app/common/configs/app.config';
+import DatabaseConfig, { DatabaseConfigType } from '@app/common/configs/database.config';
 import LoggerConfig from '@app/common/configs/logger.config';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, load: [AppConfig, LoggerConfig] })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [AppConfig, LoggerConfig] }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule.forRoot({ load: [DatabaseConfig] })],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => configService.get<DatabaseConfigType>('database') ?? {},
+    }),
+  ],
   controllers: [AppController],
   providers: [],
 })
